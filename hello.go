f1aa9ef62b1d6716fa9ba1280fb16ade0e42817c
Ashmita Bhattacharyya <ashmitab04@gmail.com>
2026-04-13 19:50:42 -0400

    go time

---
+++ hello.go
@@ -0,0 +1,8 @@
+package main // runnable program, not a library
+import ("fmt"
+"time") // formatting / printing library
+
+func main(){
+	fmt.Println("hello world")
+	fmt.Println("time=", time.Now())
+}
