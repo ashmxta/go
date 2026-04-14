c08321959f44fcf3672675a117c36c373dfbf79c
Ashmita Bhattacharyya <ashmitab04@gmail.com>
2026-04-13 20:06:58 -0400

    imports go

---
+++ imports.go
@@ -0,0 +1,7 @@
+package main
+import ("fmt")
+import("math") // can also put in same parenthesis (across different lines)
+
+func main(){
+	fmt.Printf("now you have %g problems \n", math.Sqrt(7)) // the fcn names seem to generally start with capital letters
+}
